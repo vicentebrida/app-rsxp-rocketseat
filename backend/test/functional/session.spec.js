5bad5d333ad8677df537e1c0ad8d22a65d447b85
Vicente Brida <vicentebrida@outlook.com>
2019-09-07 10:58:51 -0300

    Add authentication

---
+++ backend/test/functional/session.spec.js
@@ -0,0 +1,28 @@
+const { test, trait } = use('Test/Suite')('Session');
+
+/** @type {import('@adonisjs/lucid/src/Factory')} */
+const Factory = use('Factory');
+
+/** @type {typeof import('@adonisjs/lucid/src/Lucid/Model')} */
+const User = use('App/Models/User');
+
+trait('Test/ApiClient');
+
+test('it should return JWT token when session created', async ({ assert, client }) => {
+  const sessionPayload = {
+    email: 'vicentebrida@outlook.com',
+    password: '1234'
+  };
+
+  const user = await Factory
+    .model('App/Models/User')
+    .create(sessionPayload);
+
+  const response = await client
+    .post('/sessions')
+    .send(sessionPayload)
+    .end();
+
+  response.assertStatus(200);
+  assert.exists(response.body.token);
+});
