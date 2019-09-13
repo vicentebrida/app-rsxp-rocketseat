a292dd34deb97264fc645f9e482e4091df1e48f8
Vicente Brida <vicentebrida@outlook.com>
2019-09-12 23:44:16 -0300

    Add forgot and reset password

--- backend/start/routes.js
+++ backend/start/routes.js
@@ -17,3 +17,6 @@
 const Route = use('Route')
 
 Route.post('/sessions', 'SessionController.store');
+
+Route.post('/forgot', 'ForgotPasswordController.store');
+Route.post('/reset', 'ResetPasswordController.store');
